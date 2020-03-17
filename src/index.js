ea493133ffaf9ca9e5cd7298010967358d4adb04
Liubou Chaika <liubouchaika@gmail.com>
2020-03-18 01:29:37 +0300

    feat: add decode function

--- src/index.js
+++ src/index.js
@@ -37,9 +37,34 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
-}
+function decode (expr) {
+    let numberArray = expr.match(/.{1,10}/g); console.log(numberArray);
+    let items = []; 
+    let element=[]; 
+    for (let i = 0; i < numberArray.length; i++) {
+        let array = numberArray[i].match(/.{1,2}/g).filter(item => item !== '00');
+        let item = array.forEach( elem => {
+            if (elem =='11') {
+                element.push('-');
+            } else{
+                if (elem == '10') {
+                    element.push('.');
+                }
+            }
+        }); 
+        let it = element.join('');
+        items.push(it); element=[];
+    }
+   let fullArray = [];
+   for (let i = 0; i < items.length; i++) {
+        if (items[i] == '') {
+            fullArray.push(' ');
+        } else {
+        fullArray.push(MORSE_TABLE[items[i]]);
+        }
+   };
+   return fullArray.join('');
+};
 
 module.exports = {
     decode
